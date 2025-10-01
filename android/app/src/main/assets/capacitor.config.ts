931de6fda02550b55d9bea48824250acdbdf4cc4
Andreas <acvetne@gmail.com>
2025-10-01 10:53:51 -0400

    android functionality

---
+++ android/app/src/main/assets/capacitor.config.ts
@@ -0,0 +1,20 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const isDev = process.env.CAP_ENV === 'dev';
+
+const config: CapacitorConfig = {
+    appId: 'com.acvetne.breakroom',
+    appName: 'breakroom',
+    webDir: 'dist',
+    bundledWebRuntime: false,
+    ...(isDev
+        ? {
+            server: {
+                url: 'http://10.0.2.2:8080',
+                cleartext: true,
+            },
+        }
+    : {}),
+};
+
+export default config;
