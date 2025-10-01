a8ffd5b248a4a37746b0faf0f0d120209f027e42
Andreas <acvetne@gmail.com>
2025-10-01 15:14:23 -0400

    update debug

--- android/app/src/main/java/com/acvetne/breakroom/MainActivity.java
+++ android/app/src/main/java/com/acvetne/breakroom/MainActivity.java
@@ -1,11 +1,17 @@
 package com.acvetne.breakroom;
 
 import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+
+    // Enable WebView debugging in debug builds
+    if (android.os.Build.TYPE.equals("userdebug") || android.os.Build.TYPE.equals("eng")) {
+      WebView.setWebContentsDebuggingEnabled(true);
+    }
   }
 }
