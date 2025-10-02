0cb64e16a539d23c5f0f6490610ad081df0354e7
Andreas <acvetne@gmail.com>
2025-10-02 18:35:33 -0400

    hardware

--- android/app/src/main/java/com/acvetne/breakroom/MainActivity.java
+++ android/app/src/main/java/com/acvetne/breakroom/MainActivity.java
@@ -1,6 +1,7 @@
 package com.acvetne.breakroom;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
@@ -8,6 +9,10 @@ public class MainActivity extends BridgeActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED,
+            WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED
+    );
 
     // Enable WebView debugging in debug builds
     if (android.os.Build.TYPE.equals("userdebug") || android.os.Build.TYPE.equals("eng")) {
